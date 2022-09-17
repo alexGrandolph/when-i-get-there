7c52ab20b7b7e76601a37ff359199ce6e124ce29
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-17 13:21:57 -0600

    docs: install dependencies, move to react 17

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Login from './components/Login.js'
+
 function App() {
   return (
     <div className="App">
       <h1>Test</h1>
-      <Login />
+  
     </div>
   );
 }
