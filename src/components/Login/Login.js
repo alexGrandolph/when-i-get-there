c19488a1214348b2ecf30ea8dd79cbedb078513d
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-17 13:54:04 -0600

    feat: add html for login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,44 @@
 import React from 'react'
 
 const Login = () => {
+  
+  function handleChange() {
+
+  }
+  function handleSubmit() {
+
+  }
+  
   return (
     <div className='login-container'>
       
       <h1>Please Login to Your Account</h1>
       
-      
-      
-      
+      <div className="form-container">
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            className="form-input"
+            type="email"
+            placeholer="Email Address"
+            onChange={handleChange}
+            value={'email-state'}
+          >
+          </input>
+          <input
+            className="form-input"
+            type="password"
+            placeholer="Password"
+            onChange={handleChange}
+            value={'password-state'}
+          >
+          </input>
+          <button className="form-submit">
+            Login
+          </button>
+
+
+        </form>
+      </div>
       
       
     </div>
