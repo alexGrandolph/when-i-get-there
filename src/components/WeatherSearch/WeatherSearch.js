c289f2de1d0f66f9516558e28ec676b7912eaa0d
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-09-20 10:31:21 -0600

    feat: add form html elements for weather/city search

--- src/components/WeatherSearch/WeatherSearch.js
+++ src/components/WeatherSearch/WeatherSearch.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { useState } from 'react'
 
 const WeatherSearch = () => {
+  
+  const [cityName, setCityName] = useState("")
+  
+  function handleChange(event) {
+
+  }
+
+  function handleSubmit(event) {
+
+  }
+
   return (
-    <div>WeatherSearch</div>
+    
+    <div className="weather-search-container">
+      WeatherSearch
+      
+      <div className="form-container">
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            className="form-input"
+            type="text"
+            placeholer="Enter a City"
+            name="cityName"
+            onChange={handleChange}
+            value={cityName}
+          >
+          </input>
+          <button className="form-submit">
+            Search
+          </button>
+      
+      
+      
+      
+        </form>
+
+      </div>
+    </div>
   )
 }
 
